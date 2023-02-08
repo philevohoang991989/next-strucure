33c206f83b534f8ca2e1da1e93d0af8bdcc22264
Le Vo Hoang Phi <phi.le@bnksolution.com>
2023-02-08 16:21:34 +0700

    update code

--- src/components/common/header/routes.ts
+++ src/components/common/header/routes.ts
@@ -3,22 +3,7 @@ export const ROUTE_LIST = [
     label: 'Home',
     path: '/'
   },
-  {
-    label: 'Desitnations',
-    path: '/desitnations'
-  },
-  {
-    label: 'Hotels',
-    path: '/hotels'
-  },
-  {
-    label: 'Flights',
-    path: '/flights'
-  },
-  {
-    label: 'Bookings',
-    path: '/bookings'
-  },
+
   {
     label: 'Works',
     path: '/works'
