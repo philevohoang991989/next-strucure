80277474d49ab0a4521661bf2e26c24b8a1beece
Lê Võ Hoàng Phi <levohoangphi@HoangPhi.local>
2023-02-14 10:11:27 +0700

    loadding when click btn login

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,75 +1,92 @@
-import { LoginPayload } from '@/models'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
-import { Button, IconButton, InputAdornment } from '@mui/material'
-import { Box } from '@mui/system'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { InputField } from '../form'
+import { LoginPayload } from "@/models";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  CircularProgress,
+  IconButton,
+  InputAdornment
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { InputField } from "../form";
 
 export interface LoginFormProps {
-  onSubmit?: (payload: LoginPayload) => void
+  onSubmit?: (payload: LoginPayload) => void;
 }
 
 export function LoginForm({ onSubmit }: LoginFormProps) {
   const schema = yup.object().shape({
     username: yup
       .string()
-      .required('Please enter username')
-      .min(4, 'Username is required to have at least 4 characters'),
+      .required("Please enter username")
+      .min(4, "Username is required to have at least 4 characters"),
 
     password: yup
       .string()
-      .required('Please enter password')
-      .min(6, 'Password is required to have at least 6 characters')
-  })
-  const { control, handleSubmit } = useForm<LoginPayload>({
+      .required("Please enter password")
+      .min(6, "Password is required to have at least 6 characters"),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginPayload>({
     defaultValues: {
-      username: '',
-      password: ''
+      username: "",
+      password: "",
     },
-    resolver: yupResolver(schema)
-  })
+    resolver: yupResolver(schema),
+  });
 
-  function handleLoginSubmit(payload: LoginPayload) {
-    console.log(payload)
-    onSubmit?.(payload)
+  async function handleLoginSubmit(payload: LoginPayload) {
+    await onSubmit?.(payload);
   }
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <Box component='form' onSubmit={handleSubmit(handleLoginSubmit)}>
-      <InputField size='medium' name='username' label='Username' control={control} />
+    <Box component="form" onSubmit={handleSubmit(handleLoginSubmit)}>
       <InputField
-        type={showPassword ? 'text' : 'password'}
-        size='medium'
-        name='password'
-        label='Password'
+        size="medium"
+        name="username"
+        label="Username"
+        control={control}
+      />
+      <InputField
+        type={showPassword ? "text" : "password"}
+        size="medium"
+        name="password"
+        label="Password"
         control={control}
         InputProps={{
           endAdornment: (
-            <InputAdornment position='end'>
+            <InputAdornment position="end">
               <IconButton
-                aria-label='toggle password visibility'
+                aria-label="toggle password visibility"
                 onClick={() => setShowPassword((x) => !x)}
-                edge='end'
+                edge="end"
               >
                 {showPassword ? <VisibilityOff /> : <Visibility />}
               </IconButton>
             </InputAdornment>
-          )
+          ),
         }}
       />
       <Button
-        type='submit'
-        variant='contained'
+        disabled={isSubmitting}
+        startIcon={
+          isSubmitting ? <CircularProgress color="inherit" size="1em" /> : null
+        }
+        type="submit"
+        variant="contained"
         fullWidth
         sx={{
-          mt: 2
+          mt: 2,
         }}
       >
         Login
       </Button>
     </Box>
-  )
+  );
 }
